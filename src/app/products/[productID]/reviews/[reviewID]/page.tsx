347772dc43bc79e3ea73f8c5c1c00f83216affbd
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2023-11-30 02:41:39 +0500

    Add error handling for reviewID parameter

--- src/app/products/[productID]/reviews/[reviewID]/page.tsx
+++ src/app/products/[productID]/reviews/[reviewID]/page.tsx
@@ -1,3 +1,6 @@
+import { notFound } from "next/navigation"
+import { parse } from "path";
+
 export default function ReviewDetail( { 
     params,
 } : { 
@@ -6,6 +9,9 @@ export default function ReviewDetail( {
         reviewID : string;
      };
     }) {
+      if (parseInt(params.reviewID) < 1000) {
+          notFound();
+      }
     return (
       <h1>Review {params.reviewID } for product {params.productID} </h1>
     )
